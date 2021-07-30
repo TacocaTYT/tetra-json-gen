8272ba0db77e6d6c2ae59e6dfd083515d8fd3458
TacocaTYT <herokadendoesminecraft@gmail.com>
2021-07-30 16:08:45 -0700

    Added main menu but its really shitty rn TODO: Finish the write input template Port sadi template to other socket module types add new modules actually check if it works maybe i could implement a colorpicker hex generator GUI if i feel really masichistic one day

--- selectors/fancyClassSelector.py
+++ selectors/fancyClassSelector.py
@@ -1,2 +1,32 @@
 from generators.socket import user_input_array as socket
-socket()
+from tkinter import *
+
+
+def mm():
+    OPTIONS = [
+        "Socket"
+    ]
+
+    master = Tk()
+
+    mOS = StringVar(master)
+    mOS.set(OPTIONS[0]) # default value
+
+    w = OptionMenu(master, mOS, *OPTIONS)
+    w.pack()
+
+    def ok():
+        master.destroy()
+
+    button = Button(master, text="OK", command=ok)
+    button.pack()
+
+    mainloop()
+    return mOS.get()
+
+modGen = mm()
+
+if modGen == 'Socket':
+    socket()
+else:
+    mm()
